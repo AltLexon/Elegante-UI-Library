9795ec791c6c5752e261872b23a1bcfcde6c9488
AltLexon <xanonymusx19@gmail.com>
2024-07-07 22:39:40 -0300

    edited run script

--- run.lua
+++ run.lua
@@ -1,5 +1,5 @@
-local rbxmSuite = loadstring(game:HttpGet("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))();
+local rbxmSuite = loadstring((game :: any):HttpGet("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))();
 local project = rbxmSuite.download("AltLexon/Elegante-UI-Library@v1.0.0", "ui_library.rbxm");
+local model = rbxmSuite.launch(project);
 
--- 🚀 Launch Project
-rbxmSuite.launch(project);
+return rbxmSuite.require(model);
