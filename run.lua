fbaa3b61cae2eb3d4e714b1c3fa7a1168746060d
AltLexon <xanonymusx19@gmail.com>
2024-07-09 18:40:00 -0300

    Version support

--- run.lua
+++ run.lua
@@ -1,5 +1,15 @@
-local rbxmSuite = loadstring((game :: any):HttpGet("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))();
-local project = rbxmSuite.download("AltLexon/Elegante-UI-Library@v1.0.0", "ui_library.rbxm");
+local loadstring = loadstring :: any;
+local game = game :: any;
+
+local args = table.pack(...);
+local version = "latest";
+
+if #args > 0 and args[1] and rawget(args[1], "version") then
+    version = "v" .. tostring(args[1].version);
+end
+
+local rbxmSuite = loadstring(game:HttpGet("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))();
+local project = rbxmSuite.download("AltLexon/Elegante-UI-Library@" .. version, "ui_library.rbxm");
 local model = rbxmSuite.launch(project);
 
 return rbxmSuite.require(model);
